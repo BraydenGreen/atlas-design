99f93ed8b2f4c1c9fbe24982907b78383a09943f
BraydenGreen <brayden.p.green@gmail.com>
2017-08-10 23:00:23 -0600

    Gives create, update methods to app. New blog functionality

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,8 +4,28 @@ class BlogsController < ApplicationController
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
+    @blog = Blog.new
   end
+
+  def create
+    @blog = Blog.new(post_params)
+
+    if @blog.save
+      redirect_to blogs_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:blog).permit(:title, :body)
+  end
+
+
 end
