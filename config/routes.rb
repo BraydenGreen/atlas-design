07876f06b4df936396df700a437a7084a3b57862
BraydenGreen <brayden.p.green@gmail.com>
2017-08-13 09:01:31 -0600

    adds about me, contact, and faq pages, and styled navbar with working links.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'pages/about', to: 'pages#about'
+  get 'pages/contact', to: 'pages#contact'
+  get 'pages/faq', to: 'pages#faq'
+
   resources :blogs
 
 
